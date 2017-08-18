8699a6b15fb10240307f919a6d41b2f8e8e3893e
afiq rostam <afiqrostam@hotmail.com>
2017-08-18 12:36:08 +0800

    Create array-find-polyfill.js

---
+++ array-find-polyfill.js
@@ -0,0 +1,1 @@
+Array.prototype.find||Object.defineProperty(Array.prototype,"find",{value:function(a){"use strict";if(null==this)throw new TypeError("Array.prototype.find called on null or undefined");if("function"!=typeof a)throw new TypeError("predicate must be a function");for(var e,b=Object(this),c=b.length>>>0,d=arguments[1],f=0;f<c;f++)if(e=b[f],a.call(d,e,f,b))return e}});
