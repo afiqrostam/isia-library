edfc51048d845715bfb56c5fde18cb4e6cc1ac1a
afiq rostam <afiqrostam@hotmail.com>
2018-01-17 13:05:50 +0800

    Update array-find-polyfill.js

--- array-find-polyfill.js
+++ array-find-polyfill.js
@@ -1,11 +1,4 @@
 console.time("array-find-polyfill");
-Array.prototype.find || Object.defineProperty(Array.prototype, "find", {
-    value: function(a) {
-        "use strict";
-        if (null == this) throw new TypeError("Array.prototype.find called on null or undefined");
-        if ("function" != typeof a) throw new TypeError("predicate must be a function");
-        for (var e, b = Object(this), c = b.length >>> 0, d = arguments[1], f = 0; f < c; f++)
-            if (e = b[f], a.call(d, e, f, b)) return e
-    }
-});
+Array.prototype.find||Object.defineProperty(Array.prototype,"find",{value:function(r){"use strict";if(null==this)throw new TypeError("Array.prototype.find called on null or undefined");if("function"!=typeof r)throw new TypeError("predicate must be a function");for(var e,t=Object(this),n=t.length>>>0,o=arguments[1],i=0;n>i;i++)if(e=t[i],r.call(o,e,i,t))return e}});
+Array.prototype.findIndex||Object.defineProperty(Array.prototype,"findIndex",{value:function(r){if(null==this)throw new TypeError('"this" is null or not defined');var e=Object(this),t=e.length>>>0;if("function"!=typeof r)throw new TypeError("predicate must be a function");for(var n=arguments[1],o=0;t>o;){var i=e[o];if(r.call(n,i,o,e))return o;o++}return-1}});
 console.timeEnd("array-find-polyfill");
